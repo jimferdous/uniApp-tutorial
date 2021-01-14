6fc41bb4bd66bd71a42d4eafc326f660361f7f59
stackguardian <mferdous98@aol.com>
2021-01-14 16:42:00 -0500

    css done

---
+++ app/controllers/pages_controller.rb
@@ -0,0 +1,6 @@
+class PagesController < ApplicationController
+
+    def about #about page
+
+    end
+end
---
+++ config/routes.rb
@@ -0,0 +1,6 @@
+Rails.application.routes.draw do
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  root 'courses#index' #send the route to courses controller, root is default page
+  get 'courses/new', to: 'courses#new'
+  get 'about', to: 'pages#about' #get 'about' request and send it to the pages controller about action
+end
