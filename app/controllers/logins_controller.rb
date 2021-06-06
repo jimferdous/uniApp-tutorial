8516be4865726a73798c057ccf7d585a5ac3ac8b
stackguardian <mferdous98@aol.com>
2021-06-06 17:20:41 -0400

    flash hash

--- app/controllers/logins_controller.rb
+++ app/controllers/logins_controller.rb
@@ -7,6 +7,10 @@ class LoginsController < ApplicationController
     def create
         student = Student.find_by(email: params[:logins][:email].downcase)
         if student && student.authenticate(params[:logins][:password])
+
+        else
+            flash[:notice] = "Something was wrong with your login information"    
+
     end
 
     def destroy
