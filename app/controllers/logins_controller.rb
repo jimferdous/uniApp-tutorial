0d2fd38aca747a8b32bca2f5db4012df6cb9bef3
stackguardian <mferdous98@aol.com>
2021-06-06 17:19:37 -0400

    student create

--- app/controllers/logins_controller.rb
+++ app/controllers/logins_controller.rb
@@ -6,6 +6,7 @@ class LoginsController < ApplicationController
 
     def create
         student = Student.find_by(email: params[:logins][:email].downcase)
+        if student && student.authenticate(params[:logins][:password])
     end
 
     def destroy
