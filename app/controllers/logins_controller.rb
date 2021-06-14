7c0af1901c165d5696b3867adf1ba353f84b3c1d
stackguardian <mferdous98@aol.com>
2021-06-13 22:58:34 -0400

    student sessions

--- app/controllers/logins_controller.rb
+++ app/controllers/logins_controller.rb
@@ -7,6 +7,7 @@ class LoginsController < ApplicationController
     def create
         student = Student.find_by(email: params[:logins][:email].downcase)
         if student && student.authenticate(params[:logins][:password])
+            session[:student_id]
             flash[:notice] = "You have successfully logged in"
             redirect_to student
         else
