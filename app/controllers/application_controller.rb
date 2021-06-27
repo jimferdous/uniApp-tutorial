1c6635e4a68d9b253c2c0a739914254d3d0f5406
stackguardian <mferdous98@aol.com>
2021-06-27 14:56:52 -0400

    current user logged in

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,11 @@
 class ApplicationController < ActionController::Base
 
-@current_user = Student.find(session[:student_id]) if session[:student_id]
+    def current_user
+    @current_user = Student.find(session[:student_id]) if session[:student_id]
+    end
+
+    def logged_in?
+
+    end
 
 end
