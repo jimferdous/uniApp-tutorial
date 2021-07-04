35afc0e15c6477e085c1a2cf69b94a19cb7bf9ed
stackguardian <mferdous98@aol.com>
2021-07-04 16:36:39 -0400

    before action require user

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,8 @@
 class ApplicationController < ActionController::Base
 protect_from_forgery with: :exception
 
+    before_action :require_user
+
     helper_method :current_user, :logged_in?
 
     def current_user
